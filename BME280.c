fc11ebeefd7d0cbbb093d199ac21fb19b3b49f2d
Berat Bayram <121301223+Afebia@users.noreply.github.com>
2024-07-30 22:31:33 +0300

    Update BME280.c

--- BME280.c
+++ BME280.c
@@ -6,32 +6,66 @@
  */
 #include "BME280_STM32.h"
 
+/*
+ * Compensation words definition
+ */
 unsigned short dig_T1,dig_P1, dig_H1, dig_H3;
 signed short dig_T2, dig_T3,dig_P2, dig_P3, dig_P4, dig_P5, dig_P6, dig_P7, dig_P8, dig_P9, dig_H2,dig_H4, dig_H5, dig_H6;
 
-raw_data_t data;
-
-float Temperature, Pressure, Humidity,AltitudeP,AltitudeTP;
-
+int32_t t_fine;
+/**
+  * @brief  Software Reset to BME280
+  *
+  * @param  None
+  *
+  * @retval None
+  */
 void Reset_BME280(void){
 
 	uint8_t data = 0xB6;
-	HAL_I2C_Mem_Write(&hi2c1, BME280_ADDR, RESET_P, 1, &data, 1, HAL_MAX_DELAY);
-	HAL_Delay(200);
+	HAL_I2C_Mem_Write(&bme_i2c, BME280_ADDR, RESET_REG_ADDR, 1, &data, 1, HAL_MAX_DELAY);
+	HAL_Delay(500);
 
+	//Checking for is reset process done
 	uint8_t id;
-	HAL_I2C_Mem_Read(&hi2c1, BME280_ADDR, BME280_ID_P, 1, &id, 1, HAL_MAX_DELAY);
-	if(id != CHIP_ID_BME){
-		HAL_Delay(1000);
+	HAL_I2C_Mem_Read(&bme_i2c, BME280_ADDR, CHIP_ID_REG_ADDR, 1, &id, 1, HAL_MAX_DELAY);
+
+	//If value of id register is not equal to BME280 chip id which is 0x60, wait until equal to each other
+	while(id != CHIP_ID_BME){
+		printf("BME280-> Undefined chip id\n");
+		HAL_Delay(500);
 	}
 }
 
-//Compensation değerlerinin hesaplanması
+/**
+  * @brief  Puts the BME280 into sleep mode
+  *
+  * @param  None
+  *
+  * @retval None
+  */
+HAL_StatusTypeDef BME280_SleepMode(void){
+	uint8_t init =0;
+	init = BME280_SLEEP_MODE;
+
+	return HAL_I2C_Mem_Write(&bme_i2c, BME280_ADDR, CTRL_MEAS_REG_ADDR, 1, &init, 1, 1000);
+
+}
+
+/**
+  * @brief  Reading all compensation words from calib registers
+  *
+  * @param  None
+  *
+  * @retval None
+  */
 void Calibdata_BME280(void){
-	uint8_t CalibrationData1[25];
+	uint8_t CalibrationData1[26];
 	uint8_t CalibrationData2[7];
-	HAL_I2C_Mem_Read(&hi2c1, BME280_ADDR, CALIB_DATA1_P, 1, CalibrationData1, 25, HAL_MAX_DELAY); //0x88 ile 0xA1
-	HAL_I2C_Mem_Read(&hi2c1, BME280_ADDR, CALIB_DATA2_P, 1, CalibrationData2, 7, HAL_MAX_DELAY); //0xE1 ile 0xE7
+
+	HAL_I2C_Mem_Read(&bme_i2c, BME280_ADDR, CALIB_DATA00_25_BASEADDR, 1, CalibrationData1, 26, HAL_MAX_DELAY); //From 0x88 to 0xA1
+	HAL_I2C_Mem_Read(&bme_i2c, BME280_ADDR, CALIB_DATA26_41_BASEADDR, 1, CalibrationData2, 7, HAL_MAX_DELAY); //From 0xE1 to 0xE7
+
 	dig_T1 = (CalibrationData1[1]<<8) | CalibrationData1[0];
 	dig_T2 = (CalibrationData1[3]<<8) | CalibrationData1[2];
 	dig_T3 = (CalibrationData1[5]<<8) | CalibrationData1[4];
@@ -44,38 +78,48 @@ void Calibdata_BME280(void){
 	dig_P7 = (CalibrationData1[19]<<8) | CalibrationData1[18];
 	dig_P8 = (CalibrationData1[21]<<8) | CalibrationData1[20];
 	dig_P9 = (CalibrationData1[23]<<8) | CalibrationData1[22];
-	dig_H1 = (CalibrationData1[24]);
+	dig_H1 = (CalibrationData1[25]);
 
 	dig_H2 = (CalibrationData2[1]<<8) | CalibrationData2[0];
 	dig_H3 = (CalibrationData2[2]);
-	dig_H4 = (CalibrationData2[3]<<4) | (CalibrationData2[4] & 0x0f);
+	dig_H4 = (CalibrationData2[3]<<4) | (CalibrationData2[4] & 0x0F);
 	dig_H5 = (CalibrationData2[5]<<4) | (CalibrationData2[4]>>4);
 	dig_H6 = (CalibrationData2[6]);
-
 }
 
-void RawdataBME280(void){
+/**
+  * @brief  Reading temperature, pressure and humidity raw datas
+  *
+  * @param  None
+  *
+  * @retval None
+  */
+Raw_Data_t RawdataBME280(void){
+
 	uint8_t rawData[8];
+	Raw_Data_t data;
 
-	// 0xF7 ile 0xFE arasındaki raw dataların okunması
-	HAL_I2C_Mem_Read(&hi2c1, BME280_ADDR, PRESS_MSB_P, 1, rawData, 8, 1000);
+	HAL_I2C_Mem_Read(&bme_i2c, BME280_ADDR, RAWDATA_BASEADDR, 1, rawData, 8, 1000);
 
-	// Sıcaklık, nem ve basınç için raw datanın ayrılması
-	//press ve temp 8+8+4=20 bit, hum 8+8=16 bit
+	//Separation of raw data buffer for temperature, humidity and pressure
 	data.pressr = (rawData[0]<<12)|(rawData[1]<<4)|(rawData[2]>>4);
 	data.tempr = (rawData[3]<<12)|(rawData[4]<<4)|(rawData[5]>>4);
 	data.humr = (rawData[6]<<8)|(rawData[7]);
 
+	return data;
 }
 
-int32_t t_fine;
-
-//Sıcaklık Ölçümü
+/**
+  * @brief  Compensation formula for temperature
+  *
+  * @param  Raw temperature data
+  *
+  * @retval Processed temperature data
+  */
 int32_t BME280_measure_Temp(int32_t adc_T)
 {
-
-	Calibdata_BME280();
 	int32_t var1, var2, T;
+
 	var1 = ((((adc_T>>3) - ((int32_t)dig_T1<<1))) * ((int32_t)dig_T2)) >> 11;
 	var2 = (((((adc_T>>4) - ((int32_t)dig_T1)) * ((adc_T>>4) - ((int32_t)dig_T1)))>> 12) *((int32_t)dig_T3)) >> 14;
 	t_fine = var1 + var2;
@@ -83,12 +127,17 @@ int32_t BME280_measure_Temp(int32_t adc_T)
 	return T;
 }
 
-//Basınç Ölçümü
+/**
+  * @brief  Compensation formula for pressure
+  *
+  * @param  Raw pressure data
+  *
+  * @retval Processed pressure data
+  */
 uint32_t BME280_measure_Press(int32_t adc_P)
 {
-
-	Calibdata_BME280();
 	int64_t var1, var2, p;
+
 	var1 = ((int64_t)t_fine) - 128000;
 	var2 = var1 * var1 * (int64_t)dig_P6;
 	var2 = var2 + ((var1*(int64_t)dig_P5)<<17);
@@ -97,7 +146,7 @@ uint32_t BME280_measure_Press(int32_t adc_P)
 	var1 = (((((int64_t)1)<<47)+var1))*((int64_t)dig_P1)>>33;
 	if (var1 == 0)
 	{
-		return 0; // avoid exception caused by division by zero
+		return 0;
 	}
 	p = 1048576-adc_P;
 	p = (((p<<31)-var2)*3125)/var1;
@@ -107,12 +156,17 @@ uint32_t BME280_measure_Press(int32_t adc_P)
 	return (uint32_t)p;
 }
 
-//Nem Ölçümü
+/**
+  * @brief  Compensation formula for humidity
+  *
+  * @param  Raw humidity data
+  *
+  * @retval Processed humidity data
+  */
 uint32_t BME280_measure_Hum(int32_t adc_H)
 {
-
-	Calibdata_BME280();
 	int32_t v_x1_u32r;
+
 	v_x1_u32r = (t_fine - ((int32_t)76800));
 	v_x1_u32r = (((((adc_H << 14) - (((int32_t)dig_H4) << 20) - (((int32_t)dig_H5) *\
 			v_x1_u32r)) + ((int32_t)16384)) >> 15) * (((((((v_x1_u32r *\
@@ -126,49 +180,65 @@ uint32_t BME280_measure_Hum(int32_t adc_H)
 	return (uint32_t)(v_x1_u32r>>12);
 }
 
-//Sıcaklık,nem ve basınç ölçme fonksiyonu
-void BME280Calculation(void){
+/**
+  * @brief  Reading temperature, pressure and humidity and calculating altitude from pressure value
+  *
+  * @param  None
+  *
+  * @retval BME280 sensor datas in structure data type
+  */
+void BME280Calculation(BME280_Data_t *result)
+{
+	Raw_Data_t rawData = RawdataBME280();
 
-	RawdataBME280();
+	Calibdata_BME280();
 
-	//Sıcaklık,nem ve basınç ölçme
-		Temperature = (BME280_measure_Temp(data.tempr))/100.0;//Derece
+	result->Temperature = (BME280_measure_Temp(rawData.tempr))/100.0;	//Degress
+	result->Pressure = (BME280_measure_Press(rawData.pressr))/25600.0;	//hPa
+	result->Humidity = (BME280_measure_Hum(rawData.humr))/1024.0;		//%RH
 
-		Pressure = (BME280_measure_Press(data.pressr))/25600.0;//hPa
+	result->AltitudeP = 44330*(1-pow(result->Pressure/1013.25,1/5.255)); 	/*Calculation of altitude parameter in meters with
+													simplified atmospheric pressure formula*/
 
-		Humidity = (BME280_measure_Hum(data.humr))/1024.0;//%RH
-
-		AltitudeP = 44330*(1-pow(Pressure/1013.25,1/5.255)); //Metre cinsinden yükseklik sadece basınç verisi ile yükseklik
-
-		AltitudeTP = ((log(Pressure/1013.25)*(288.16)*(-8.314))/(28.97*9.81))*1000; // Metre cinsinden
-		                                   	   	   	   	   	   	   	   	   	   	   	// sıcaklık ve basınç verisi ile yükseklik
+	result->AltitudeTP = ((log(result->Pressure/1013.25)*(288.16)*(-8.314))/(28.97*9.81))*1000; /*Calculation of altitude parameter in
+																		                    meters with atmospheric pressure formula*/
 }
 
-//İnitilization
-void BME280Init(oversampling_t oversamplingT , oversampling_t  oversamplingP,
-		oversampling_t oversamplingH,sensor_mode_t mode,standby_t t_sb,filter_t filter){
+/**
+  * @brief  Initialization of BME280
+  *
+  * @param  BME280Init argument to a BME280_Init_t structure that contains
+  *         the configuration information for the BME280 device.
+  *
+  * @retval None
+  */
+void BME280Init(BME280_Init_t BME280Init){
 
-	uint8_t init;
+	uint8_t init=0;
 
-	init = SLEEP; //Config registerı Sleep modunda değiştirilebilir.
-	HAL_I2C_Mem_Write(&hi2c1, BME280_ADDR, CTRL_MEAS_P, 1, &init, 1, 1000);
+	//Setting it to sleep mode because the config register can only be changed while the BME280 is in sleep mode
+	if(BME280_SleepMode() == HAL_OK){
+
+		printf("BME280 was put into sleep mode so that the config register could be set.!\n");
+		//Configuration of config register which is control standby time, filter and SPI 3-wire interface
+		init = ((BME280Init.T_StandBy << 5) | (BME280Init.Filter << 2) | (BME280Init.SPI_EnOrDıs << 0));
+		HAL_I2C_Mem_Write(&bme_i2c, BME280_ADDR, CONFIG_REG_ADDR, 1, &init, 1, 1000);
+		HAL_Delay (100);
+		init=0;
+	}
+
+	//Configuration of ctrl_hum register which is control oversamplig of Humidity
+	init = ((BME280Init.OverSampling_H << 0) & 0x7);
+	HAL_I2C_Mem_Write(&bme_i2c, BME280_ADDR, CTRL_HUM_REG_ADDR, 1, &init, 1, 1000);
 	HAL_Delay (100);
+	init=0;
 
-	//Config register, standby time, IIR filter, ve SPI 3-wire enable biti(1 ise aktif)
-	init = (t_sb << 5)| (filter << 2) | 0x00;
-	HAL_I2C_Mem_Write(&hi2c1, BME280_ADDR, CONFIG_P, 1, &init, 1, 1000);
+	//Configuration of ctrl_meas register which is control oversamplig of Temperature-Pressure and device mode
+	init = (BME280Init.OverSampling_T << 5) | (BME280Init.OverSampling_P << 2) | BME280Init.Mode;
+	HAL_I2C_Mem_Write(&bme_i2c, BME280_ADDR, CTRL_MEAS_REG_ADDR, 1, &init, 1, 1000);
 	HAL_Delay (100);
+	init=0;
 
-	//CTRL_hum register, Nem oversampling
-	init = oversamplingH;
-	HAL_I2C_Mem_Write(&hi2c1, BME280_ADDR, CTRL_HUM_P, 1, &init, 1, 1000);
-	HAL_Delay (100);
-
-
-	//CTRL_meas register, Sıcaklık ve basınç oversampling ve mode select
-	init = (oversamplingT << 5) | (oversamplingP << 2) | mode;
-	HAL_I2C_Mem_Write(&hi2c1, BME280_ADDR, CTRL_MEAS_P, 1, &init, 1, 1000);
-	HAL_Delay (100);
-
+	printf("BME280 Initialization process is done!\n");
 }
 
